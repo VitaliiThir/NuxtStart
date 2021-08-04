5de482c066fc1593a0b79e7b0cb37b9a7a4e48d9
VitalyGitHub <Vitslikus865@gmail.com>
2021-08-04 14:57:59 +0400

    Last

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,29 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt',
+    title: 'NuxtStart',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ru'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'msapplication-config', content: '/favicons/browserconfig.xml' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', color: '#5bbad5', href: '/favicons/safari-pinned-tab.svg' },
+      { rel: 'shortcut icon', href: '/favicons/favicon.ico' }
     ]
   },
 
-  loading: {color: '#3B8070'},
+  loading: { color: '#3B8070' },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
@@ -24,8 +31,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,8 +39,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    '@nuxtjs/svg-sprite'
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,11 +52,16 @@ export default {
         optipng: { optimizationLevel: 5 },
         gifsicle: { optimizationLevel: 2 }
       }
-    ]
+    ],
+    '@nuxtjs/svg-sprite',
+    'nuxt-rfg-icon',
+    '@nuxtjs/manifest',
   ],
 
-  spriteSvgLoader: {
-    symbolId: 'icon-[name]'
+  'rfg-icon': {
+    static: true,
+    staticPath: '/favicons/',
+    masterPicture: 'static/favicon.svg',
   },
 
   svgSprite: {
@@ -64,7 +74,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend (config, {isDev, isClient}) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
